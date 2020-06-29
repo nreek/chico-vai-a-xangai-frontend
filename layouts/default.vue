53159c179174d944c5c9aa61f2aeb2df79473097
Henrique Marques <henrique1_3@hotmail.com>
2020-06-29 01:59:55 -0300

    Initial Setup

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Header></Header>
+        <Nuxt />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '~/components/Header.vue'
+
+export default Vue.extend({
+    components :  { Header }
+})
+</script>
+
+<style lang="scss">
+</style>
